4122f2d87569d2f061ee72a2936ea433bd5f2ebf
Rahul <nayakrahul085@gmail.com>
2025-04-23 19:41:58 +0530

    2 version of data is saved

--- mycode.py
+++ mycode.py
@@ -7,7 +7,13 @@ data={
     'City': ['New York', 'Los Angeles', 'Chicago']
 }
 
+
+
+
 df=pd.DataFrame(data)
+
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 
 data_dir='data'
